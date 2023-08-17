c9c1cb32e216c34980747e964b109ad88c22f132
nluuarch@gmail.com <nluuarch@gmail.com>
2023-08-17 12:36:20 -0700

    project setup with firebase, hooks, and context

---
+++ src/firebase/config.js
@@ -0,0 +1,25 @@
+import firebase from "firebase";
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// object that is used to connect firebase to the backend
+const firebaseConfig = {
+  apiKey: "AIzaSyD6TqMimv4l3tkmI42Uwrk5AWV7K5iWsMA",
+  authDomain: "mypromanagement-3617d.firebaseapp.com",
+  projectId: "mypromanagement-3617d",
+  storageBucket: "mypromanagement-3617d.appspot.com",
+  messagingSenderId: "53377633137",
+  appId: "1:53377633137:web:49094040fa8b587b3144d0"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+// timestamp
+const timestamp = firebase.firestore.Timestamp;
+
+export { projectFirestore, projectAuth, timestamp }
