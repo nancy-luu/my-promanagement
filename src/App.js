23a503af6eadbe3b260c9bbec621e4b7d2442111
nluuarch@gmail.com <nluuarch@gmail.com>
2023-08-17 13:41:56 -0700

    setting up pages and components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+//pages and components
+import Dashboard from './pages/dashboard/Dashboard';
+import Create from './pages/create/Create';
+import Login from './pages/login/Login';
+import Project from './pages/project/Project';
+import Signup from './pages/signup/Signup';
+
+//styles
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-
+      <Dashboard />
+      <Create />
+      <Login />
+      <Project />
+      <Signup />
     </div>
   );
 }
