33f7d0e3ab351f5d705c3a42ac7f1f6c50a055f2
nluuarch@gmail.com <nluuarch@gmail.com>
2023-10-11 14:17:51 -0700

    overview complete

--- src/pages/dashboard/Overview.js
+++ src/pages/dashboard/Overview.js
@@ -6,26 +6,28 @@ import { useMyProjects } from '../../hooks/useMyProjects';
 // styles
 import './Overview.css'
 
-const Overview = () => {
-    const { user } = useAuthContext();
-    const { error: projectError, documents: projectDocuments } = useCollection('projects')
-    
+const Overview = () => {    
     const { myProjects } = useMyProjects();
+    // console.log(myProjects)
+    
+    const projectCount = myProjects.length;
+    const completedProject = myProjects.filter(project => project.isCompleted)
+    const inProgressProjects = myProjects.filter(project => project.comments && project.comments.length > 0);
 
 
   return (
     <div className="overview-container">
         <div className="total-stack">
             <h3>Total Projects</h3>
-            <h1>15</h1>
+            <h1>{projectCount ? projectCount : '-'}</h1>
         </div>
         <div className="completed-stack">
             <h3>Completed</h3>
-            <h1>8</h1>
+            <h1>{completedProject.length ? completedProject.length : '-'}</h1>
         </div>
         <div className="progress-stack">
             <h3>In Progress</h3>
-            <h1>11</h1>
+            <h1>{inProgressProjects.length ? inProgressProjects.length : '-'}</h1>
         </div>
     </div>
   )
