5508439a51ab5f8a2d6afd22ae95b453136a31d4
nluuarch@gmail.com <nluuarch@gmail.com>
2023-11-13 16:37:25 -0800

    edited options of assigned users

--- src/components/UpdateModal.js
+++ src/components/UpdateModal.js
@@ -60,9 +60,17 @@ const UpdateModal = ({ project }) => {
       const options = documents.map((user) => {
         return { value: user, label: user.displayName, img: user.photoURL };
       });
-      setUsers(options);
+
+      const filteredOptions = options.filter((option) => {
+        return !assignedUsers.some((selectedUser) =>
+          selectedUser.value.id === option.value.id
+        );
+      });
+  
+      setUsers(filteredOptions);
     }
-  }, [documents]);
+
+  }, [documents, assignedUsers]);
 
   //   formatting for Assigned User Select Default State
   useEffect(() => {
@@ -81,60 +89,68 @@ const UpdateModal = ({ project }) => {
   const handleSave = async (e) => {
     e.preventDefault();
     setFormError(null);
-
-    if (!category) {
-      setFormError("Please select category.");
-      return;
-    }
-    if (assignedUsers.length === 0) {
+  
+    try {
+      if (!category) {
+        setFormError("Please select category.");
+        return;
+      }
+      if (assignedUsers.length === 0) {
         setFormError("Please assign to at least 1 team member.");
         return;
-    }
-
-    const createdBy = {
-      displayName: user.displayName,
-      photoURL: user.photoURL,
-      id: user.uid,
-    };
-
-    // creating a simplified array of obects from useAuthContext with the following properties
-    const assignedUsersList = assignedUsers.map((u) => {
-      return {
-        displayName: u.value.displayName,
-        photoURL: u.value.photoURL,
-        id: u.value.id,
+      }
+  
+      const createdBy = {
+        displayName: user.displayName,
+        photoURL: user.photoURL,
+        id: user.uid,
       };
-    });
-
-    await updateDocumentSummary(project.id, {
-      name,
-      details,
-      category: category.value,
-      dueDate: timestamp.fromDate(convertUTC(new Date(dueDate))),
-      assignedUsersList: assignedUsersList,
-    });
-
-    console.log("hitting update");
-    console.log("due date: ------>" + dueDate);
-
-    if (!response.error) {
-      console.log("this is the response id:" + response.id);
-      setAppear(false);
-      console.log("\n");
-      console.log(
-        "Project duedate from update: " + project.dueDate.toDate().toString()
-      );
-      console.log("\n");
-      history.push(`/projects/${project.id}`);
-
-
+  
+      // creating a simplified array of objects from useAuthContext with the following properties
+      const assignedUsersList = assignedUsers.map((u) => {
+        return {
+          displayName: u.value.displayName,
+          photoURL: u.value.photoURL,
+          id: u.value.id,
+        };
+      });
+  
+      const updateResult = await updateDocumentSummary(project.id, {
+        name,
+        details,
+        category: category.value,
+        dueDate: timestamp.fromDate(convertUTC(new Date(dueDate))),
+        assignedUsersList: assignedUsersList,
+      });
+  
+      // Check the update result
+      if (updateResult && updateResult.error) {
+        // Handle the case where there's an error in the update result
+        console.error("Error updating document:", updateResult.error);
+        setFormError("An error occurred while updating the document.");
+      } else {
+        // Success case
+        console.log("Update successful");
+        setAppear(false);
+        console.log("\n");
+        console.log(
+          "Project due date from update: " + project.dueDate.toDate().toString()
+        );
+        console.log("\n");
+        history.push(`/projects/${project.id}`);
+      }
+    } catch (error) {
+      // Handle other potential errors that may occur
+      console.error("An unexpected error occurred:", error);
+      setFormError("An unexpected error occurred.");
     }
-    console.log("\n")
-    console.log(assignedUsers)
-  //   console.log(assignedUsers.forEach(user => console.log(user.value)))
-    console.log('\n')
-
+  
+    console.log("\n");
+    console.log(assignedUsers);
+    //   console.log(assignedUsers.forEach(user => console.log(user.value)))
+    console.log("\n");
   };
+  
 
 
   return (
@@ -246,74 +262,3 @@ const UpdateModal = ({ project }) => {
 };
 
 export default UpdateModal;
-
-/**
- * 
- * <label>
-                <p>Due Date:</p>
-                <input
-                  required
-                  type="date"
-                  onChange={(e) => setDueDate(e.target.value)}
-                  value={dueDate}
-                ></input>
-              </label>
-          <div className="category-assign-container">
-            <label>
-              <p>Category:</p>
-              <Select
-                className="category-select"
-                value={category}
-                options={categories}
-                onChange={(option) => setCategory(option)}
-                // select customize only here - not in css
-                theme={(theme) => ({
-                  ...theme,
-                  borderRadius: "5px",
-                  colors: {
-                    ...theme.colors,
-                    text: "orange",
-                    primary25: "orange",
-                    primary: "orange",
-                  },
-                })}
-                placeholder={category}
-              />
-            </label>
-            <label>
-              <p>Assign to:</p>
-              <Select
-                isMulti
-                value={assignedUsers}
-                onChange={(option) => setAssignedUsers(option)}
-                options={users}
-                formatOptionLabel={(user) => (
-                  <div className="assigned-user-option">
-                    <img src={user.img} className="avatar" alt="user-avatar" />
-                    <p>{user.label}</p>
-                  </div>
-                )}
-                // select customize only here - not in css
-                theme={(theme) => ({
-                  ...theme,
-                  borderRadius: "5px",
-                  colors: {
-                    ...theme.colors,
-                    text: "orange",
-                    primary25: "orange",
-                    primary: "orange",
-                  },
-                })}
-              />
-            </label>
-          </div>
-          <label>
-            <p>Details:</p>
-            <textarea
-              required
-              type="text"
-              onChange={(e) => setDetails(e.target.value)}
-              value={details}
-            ></textarea>
-          </label>
- */
