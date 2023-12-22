efd768f4c73662c7613b58beee7a3972080cf0f0
nluuarch@gmail.com <nluuarch@gmail.com>
2023-12-21 18:19:04 -0800

    meeting info complete

--- src/pages/calendar/CreateMeeting.js
+++ src/pages/calendar/CreateMeeting.js
@@ -87,78 +87,80 @@ const CreateMeeting = () => {
     }
 
   return (
-    <div className="create-event-container">
-        <h3>Shedule New Meeting</h3>
-        <form onSubmit={handleSubmitMeeting}>
-            <label>
-                <input 
-                    placeholder="Add title"
-                    required 
-                    type="text" 
-                    onChange={(e) => setTitle(e.target.value)}
-                    value={title}
-                />
+    <div className="create-event-wrapper">
+      <div className="create-event-container">
+          <h3>Shedule New Meeting</h3>
+          <form onSubmit={handleSubmitMeeting}>
+              <label>
+                  <input 
+                      placeholder="Add title"
+                      required 
+                      type="text" 
+                      onChange={(e) => setTitle(e.target.value)}
+                      value={title}
+                  />
+              </label>
+              <label>
+                  <div className="start-end-input-container">
+                      <Flatpickr
+                          value={start}
+                          options={{
+                              enableTime: true,
+                              altInput: true,
+                          }}
+                          onChange={(selectedDates) => setStart(selectedDates[0])}
+                          />
+                      <p>to</p>
+                      <Flatpickr
+                          value={end}
+                          options={{
+                              enableTime: true,
+                              altInput: true,
+                          }}
+                          onChange={(selectedDates) => setEnd(selectedDates[0])}
+                          />
+                  </div>
+              </label>
+              <label>
+                  <textarea 
+                      placeholder="Add description"
+                      required 
+                      type="text" 
+                      onChange={(e) => setDescription(e.target.value)}
+                      value={description}
+                  />
+              </label>
+              <label>
+              <span>Invite:</span>
+              <Select
+                isMulti
+                value={guests}
+                onChange={(option) => setGuests(option)}
+                options={users}
+                formatOptionLabel={user => (
+                  <div className="assigned-user-option">
+                    <img src={user.img} className="avatar" alt="user-avatar" />
+                    <p>{user.label}</p>
+                  </div>
+                )}
+                // select customize only here - not in css 
+                theme={(theme) => ({
+                  ...theme,
+                  borderRadius: '5px',
+                  colors: {
+                  ...theme.colors,
+                    text: 'orangered',
+                    primary25: 'orange',
+                    primary: 'orange',
+                  },
+                })}
+              />
             </label>
-            <label>
-                <div className="start-end-input-container">
-                    <Flatpickr
-                        value={start}
-                        options={{
-                            enableTime: true,
-                            altInput: true,
-                        }}
-                        onChange={(selectedDates) => setStart(selectedDates[0])}
-                        />
-                    <p>to</p>
-                    <Flatpickr
-                        value={end}
-                        options={{
-                            enableTime: true,
-                            altInput: true,
-                        }}
-                        onChange={(selectedDates) => setEnd(selectedDates[0])}
-                        />
-                </div>
-            </label>
-            <label>
-                <textarea 
-                    placeholder="Add description"
-                    required 
-                    type="text" 
-                    onChange={(e) => setDescription(e.target.value)}
-                    value={description}
-                />
-            </label>
-            <label>
-            <span>Invite:</span>
-            <Select
-              isMulti
-              value={guests}
-              onChange={(option) => setGuests(option)}
-              options={users}
-              formatOptionLabel={user => (
-                <div className="assigned-user-option">
-                  <img src={user.img} className="avatar" alt="user-avatar" />
-                  <p>{user.label}</p>
-                </div>
-              )}
-              // select customize only here - not in css 
-              theme={(theme) => ({
-                ...theme,
-                borderRadius: '5px',
-                colors: {
-                ...theme.colors,
-                  text: 'orangered',
-                  primary25: 'orange',
-                  primary: 'orange',
-                },
-              })}
-            />
-          </label>
-          <button className="btn">Add</button>
+            <button className="btn">Add</button>
 
-          {formError && <p className="error">{formError}</p>}
-        </form>
+            {formError && <p className="error">{formError}</p>}
+          </form>
+      </div>
     </div>
   )
 }
--- src/pages/meeting/MeetingInfo.js
+++ src/pages/meeting/MeetingInfo.js
@@ -8,6 +8,11 @@ import { useHistory } from "react-router-dom";
 // components
 import Avatar from "../../components/Avatar"
 import CheckIcon from "../../assets/check-icon.png"
+import DetailIcon from "../../assets/detail-icon.png"
+import TimeIcon from "../../assets/time-icon.png"
+import MeetingIcon from "../../assets/meeting-icon.png"
+import DownArrow from "../../assets/down-arrow.png"
+
 
 // styles
 import './MeetingInfo.css'
@@ -15,6 +20,7 @@ import './MeetingInfo.css'
 const MeetingInfo = () => {
   const [start, setStart] = useState()
   const [end, setEnd] = useState()
+  const [showGuests, setShowGuests] = useState(false)
   const { id } = useParams();
   const { user } = useAuthContext();
   const { document: meeting, error} = useDocument("meetings", id)
@@ -71,45 +77,71 @@ const MeetingInfo = () => {
 
     if (!response.error) {
       console.log(response)
-      console.log("this is the response id: " + response.id);
+      console.log("this is the response id: " + response.document.id);
       console.log(meeting)
       history.push(`/calendar`);
     }
   }
 
+  const handleShowGuests = () => {
+    if(!showGuests){
+      setShowGuests(true)
+    } else {
+      setShowGuests(false)
+    }
+  }
+
 
   return (
-    <>
+    <div className="meeting-info-wrapper">
       {meeting && 
         <div className="meeting-info-container">
           <h3>{meeting.title}</h3>
-          <p>{meeting.description}</p>
-          <div className="meeting-duration">
+          <div className="detail-section">
+            <img src={DetailIcon} alt="detail icon"/>
+            <p>{meeting.description}</p>
+          </div>
+          <div className="time-section">
+            <img src={TimeIcon} alt="time icon"/>
             <p>{start} - {end}</p>
           </div>
-          <div className="guest-count">
-            <h4>{meeting.guestsInvitedList.length} guests</h4>
-            <p>{meeting.guestsInvitedList.filter(guest => guest.accepted).length > 0 && 
-                <>
-                  {meeting.guestsInvitedList.filter(guest => guest.accepted).length} yes, 
-                </>
-              } {meeting.guestsInvitedList.filter(guest => !guest.accepted).length} awaiting</p>
-          </div>
-          <div className="meeting-guests-container">
-            {meeting.guestsInvitedList.map(guest => 
-              <div className="single-guest-container">
-                <div className="guest-image-container">
-                  <Avatar src={guest.photoURL}/>
-                  {guest.accepted && <img className="check-icon" src={CheckIcon} alt="check icon"></img>}
-                </div>
-                <div className="guests-name">
-                  <p>{guest.displayName}</p>
-                  {guest.id === meeting.createdBy.id ? <h5>Organizer</h5> : <></>}
-                </div>
+          <div className="guest-section" onClick={handleShowGuests}>
+            <div className="guest-count-wrapper">
+            <img src={MeetingIcon} alt="meeting guest icon" className="meeting-icon"/>
+              <div className="guest-count">
+                <h4>{meeting.guestsInvitedList.length} guests</h4>
+                <p>{meeting.guestsInvitedList.filter(guest => guest.accepted).length > 0 && 
+                    <>
+                      {meeting.guestsInvitedList.filter(guest => guest.accepted).length} yes
+                    </>
+                    }
+                    {meeting.guestsInvitedList.filter(guest => !guest.accepted).length > 0 && 
+                      <>
+                        , {meeting.guestsInvitedList.filter(guest => !guest.accepted).length} awaiting
+                      </>
+                    } 
+                </p>
               </div>
-            )}
+            </div>
+            <img src={DownArrow} alt="down arrow" className={`arrow ${showGuests ? 'up-arrow' : ''}`}/>
           </div>
-          <div className="response">
+          {showGuests && 
+            <div className="meeting-guests-container">
+              {meeting.guestsInvitedList.map(guest => 
+                <div className="single-guest-container">
+                  <div className="guest-image-container">
+                    <Avatar src={guest.photoURL}/>
+                    {guest.accepted && <img className="check-icon" src={CheckIcon} alt="check icon"></img>}
+                  </div>
+                  <div className="guests-name">
+                    <p>{guest.displayName}</p>
+                    {guest.id === meeting.createdBy.id ? <h5>Organizer</h5> : <></>}
+                  </div>
+                </div>
+              )}
+            </div>
+          }
+          <div className="response-section">
             <div>
             <h4>Going?</h4>
             </div>
@@ -120,7 +152,7 @@ const MeetingInfo = () => {
           </div>
         </div>
       }
-    </>
+    </div>
   )
 }
 
